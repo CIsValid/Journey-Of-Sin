14ab4bdf885d4a97d140793b861da649ef0c9180
TheDevArray <christian.kollbaer@gmail.com>
2020-08-28 14:17:20 +0200

    dialogue

--- Assets/Scripts/Enemies/GroundedPatroler/GroundedPatroler.cs
+++ Assets/Scripts/Enemies/GroundedPatroler/GroundedPatroler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SocialPlatforms;
@@ -7,14 +8,23 @@ using UnityEngine.SocialPlatforms;
 [RequireComponent(typeof(NavMeshAgent))]
 public class GroundedPatroler : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject target;
     public float patrolSpeed = 5;
     public float chaseSpeed = 9f;
     public float patrolWaitTimer = 1f;
     public float chaseWaitTimer = 5f; 
     public float health = 100;
 
+    public Transform[] patrolWayPoints;
+
+    private EnemySight enemySight;
+    private NavMeshAgent nav;
+    private Transform player;
+    private PlayerManager playerHealth; // to access player health
+
+    private float chaseTimer;
+    private float patrolTimer;
+
+    private int wayPointIndex;
     private int treasureCheck;
 
     public bool hasBeenRobbed;
@@ -25,6 +35,10 @@ public class GroundedPatroler : MonoBehaviour
 
     private void Awake()
     {
+        enemySight = GetComponent<EnemySight>();
+        nav = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerHealth = player.GetComponent<PlayerManager>();
         treasureCheck = Random.Range(0, 10);   
     }
 
@@ -35,6 +49,8 @@ public class GroundedPatroler : MonoBehaviour
 
     private void Update()
     {
+
+
         if(hasTreasure)
         {
             // If player steals hasTresure goes back to false
