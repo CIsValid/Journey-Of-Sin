0a1c7adfa12354e837b9f570af1637a4a0fe717b
TheDevArray <christian.kollbaer@gmail.com>
2020-08-28 15:43:49 +0200

    text

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,18 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player;
+    public GameObject playerCamera;
+    public GameObject dialogueCamera;
+    public GameObject canvas;
+    public Text text;
+
+    public List<string> dialogueList = new List<string>();
+
+    public bool hasDialogueBeenHad;
+
+    private void Update()
     {
-        
+
+        if(hasDialogueBeenHad)
+        {
+            EnableAllControls();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(!hasDialogueBeenHad)
+        {
+            if (other.CompareTag("Player"))
+            {
+                canvas.SetActive(true);
+
+                if (dialogueCamera != null)
+                {
+                    DisableAllControls();
+
+                    //activate Camera
+                    playerCamera.SetActive(false);
+                    dialogueCamera.SetActive(true);
+                }
+
+                DisableAllControls();
+
+            }
+        }
+
+    }
+
+    public void DisableAllControls()
+    {
+        player.GetComponent<GamepadChecker>().enabled = false;
+        player.GetComponent<PlayerController>().enabled = false;
+        player.GetComponent<PlayerControllerPs4>().enabled = false;
+        player.GetComponent<PlayerControllerXbox>().enabled = false;
+
+        playerCamera.GetComponent<CameraController>().enabled = false;
+        playerCamera.GetComponent<CameraControllerConsole>().enabled = false;
+        playerCamera.GetComponent<CameraControllerPs>().enabled = false;
+    }
+
+    public void EnableAllControls()
+    {
+        playerCamera.SetActive(true);
+        dialogueCamera.SetActive(false);
+
+        canvas.SetActive(false);
+
+        player.GetComponent<GamepadChecker>().enabled = true;
+        player.GetComponent<PlayerController>().enabled = true;
+        player.GetComponent<PlayerControllerPs4>().enabled = true;
+        player.GetComponent<PlayerControllerXbox>().enabled = true;
+
+        playerCamera.GetComponent<CameraController>().enabled = true;
+        playerCamera.GetComponent<CameraControllerConsole>().enabled = true;
+        playerCamera.GetComponent<CameraControllerPs>().enabled = true;
+
+        Destroy(this.gameObject);
+
     }
 }
---
+++ Assets/Scripts/Dialogue/TypeWriterScript/TextTypeWriter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SocialPlatforms;
+
+// attach to UI Text component (with the full text already there)
+
+public class TextTypeWriter: MonoBehaviour
+{
+
+	Text txt;
+	string story;
+	public float speed;
+
+	void Awake()
+	{
+		txt = GetComponent<Text>();
+		story = txt.text;
+		txt.text = "";
+
+		StartCoroutine("PlayText");
+	}
+
+	IEnumerator PlayText()
+	{
+		foreach (char c in story)
+		{
+			txt.text += c;
+			yield return new WaitForSeconds(speed);
+		}
+	}
+
+}
