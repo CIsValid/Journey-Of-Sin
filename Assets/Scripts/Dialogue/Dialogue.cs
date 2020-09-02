e9f9c8108f468bfa42d2d6d5d45bcca3796a6bce
TheDevArray <christian.kollbaer@gmail.com>
2020-09-02 16:44:48 +0200

    Fixing dialogue

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -11,6 +11,79 @@ public class Dialogue : MonoBehaviour
     public List<string> goodDialogueHolder = new List<string>();
     public List<string> badDialogueHolder = new List<string>();
 
-    private bool affectStats;
-    private bool goodOrBadDialogue;
+    public int pointsToGive;
+
+    public int pointsForGoodDialogue;
+
+    private int numberOfSentances;
+
+    public bool affectStats;
+    public bool goodOrBadDialogue;
+
+    private DialogueController dialogueController;
+
+    private BranchingStats branchingStats;
+
+    private void Start()
+    {
+        dialogueController = DialogueController.instance;
+        branchingStats = BranchingStats.instance;
+
+        switch (goodOrBadDialogue)
+        {
+            case true:
+                numberOfSentances = goodDialogueHolder.Count + badDialogueHolder.Count;
+                break;
+
+            case false:
+                numberOfSentances = dialogueHolder.Count;
+                break;
+        }
+    }
+
+    public void Update()
+    {
+        dialogueController.DisableAllControls();
+
+        if(goodOrBadDialogue)
+        {
+            if(branchingStats.goodPoints >= pointsForGoodDialogue)
+            {
+                for (int i = 0; i < goodDialogueHolder.Count; i++)
+                {
+                    switch (i)
+                    {
+                        case 1:
+
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < badDialogueHolder.Count; i++)
+                {
+                    switch (i)
+                    {
+                        case 1:
+
+                            break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < dialogueHolder.Count; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+
+                        break;
+                }
+            }
+        }
+    }
+
 }
