4745253a9fc42020eae18d74681ecb1ec118ef94
TheDevArray <christian.kollbaer@gmail.com>
2020-09-04 15:41:16 +0200

    Flying Controller

--- Assets/Scripts/FlyingController/TestFlightController.cs
+++ Assets/Scripts/FlyingController/TestFlightController.cs
@@ -4,40 +4,120 @@ using UnityEngine;
 
 public class TestFlightController : MonoBehaviour
 {
-    public CharacterController controller;
-    Animator anim;
-    public float speed = 8.0f;
-    public float rotationSpeed = 100f;
-
+    public Vector3 upwardsAcceleration;
     public Vector3 hoverGravity;
+    public float walkSpeed = 2;
+	public float runSpeed = 6;
+	public float gravity = -12;
+	[Range(0, 1)]
+	public float airControlPercent;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // anim = this.GetComponent<Animator>();
-    }
+	public float turnSmoothTime = 0.2f;
+	float turnSmoothVelocity;
 
-    // Update is called once per frame
+	public float speedSmoothTime = 0.1f;
+	float speedSmoothVelocity;
+	float currentSpeed;
+	float velocityY;
+
+	Animator animator;
+	Transform cameraT;
+	CharacterController controller;
+
+    public float timer = 5f;
+
+	void Start()
+	{
+		//animator = GetComponent<Animator>();
+		cameraT = Camera.main.transform;
+		controller = GetComponent<CharacterController>();
+	}
+
     void Update()
     {
+
+        // Ground Check
         if(controller.isGrounded)
         {
+            timer = 5f;
             PlayerManager.instance.isFlying = false;
         }
-
-        
-
-        if(Input.GetKey(KeyCode.Space))
-        {
-            // flap with wings anim.SetBool("Flapping", true)
-            transform.Translate(0,0.03f,0);
+        else{
+            timer -= Time.deltaTime;
         }
-        else
-        {
-            // Stop flapping wings anim.SetBool("Flapping", false)
-        }
-        
+
+        // input
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+        Move(inputDir, running);
+
+        Higher();
+
+        Lower();
+
         controller.Move(hoverGravity * Time.deltaTime);
 
+        // animator
+        //float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
+        //animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+
+    }
+
+	void Move(Vector2 inputDir, bool running)
+	{
+		if (inputDir != Vector2.zero)
+		{
+			float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+		}
+
+		float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+		currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+
+		velocityY += Time.deltaTime * gravity;
+		Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+		controller.Move(velocity * Time.deltaTime);
+		currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+	}
+
+	float GetModifiedSmoothTime(float smoothTime)
+	{
+		if (controller.isGrounded)
+		{
+			return smoothTime;
+		}
+
+		if (airControlPercent == 0)
+		{
+			return float.MaxValue;
+		}
+		return smoothTime / airControlPercent;
+	}
+    void Higher()
+    {
+        if(timer >= 0)
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                // flap with wings anim.SetBool("Flapping", true)
+                controller.Move(upwardsAcceleration * Time.deltaTime);
+            }
+
+        }
+    }
+
+    void Lower()
+    {
+        if(Input.GetKey(KeyCode.LeftControl)) 
+        {
+            // Stop flapping wings anim.SetBool("Flapping", false)
+            controller.Move(-upwardsAcceleration * Time.deltaTime);
+
+        }
+        
     }
 }
