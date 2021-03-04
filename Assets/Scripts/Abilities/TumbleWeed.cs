67309b44d979e0a4006aef2b5407fe70d43fbbbd
TheDevArray <Christian.kollbaer@gmail.com>
2021-03-04 17:44:06 +0100

    VFXEdit

--- Assets/Scripts/Abilities/TumbleWeed.cs
+++ Assets/Scripts/Abilities/TumbleWeed.cs
@@ -5,57 +5,20 @@ using UnityEngine;
 
 public class TumbleWeed : MonoBehaviour
 {
-    /*public float timeBeforeExplotion = 4;
-    public float timeBeforeDestruction = 2;
-
-    private MeshRenderer meshRenderer;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        meshRenderer = this.GetComponent<MeshRenderer>();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (timeBeforeExplotion >= 0)
-        {
-            timeBeforeExplotion -= Time.deltaTime;
-
-        }
-        else
-        {
-            
-            if (timeBeforeDestruction >= 0)
-            {
-                timeBeforeDestruction -= Time.deltaTime;
-                meshRenderer.enabled = false;
-
-            }
-            else
-            {
-                Destroy(this.gameObject);
-            }
-
-        }
-    }
-
-   private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Enemy"))
-        {
-            Destroy(other.gameObject);
-        }
-    }*/
-
     public GameObject explotionBuildUpVFX;
     public GameObject TumbleWeedExplotionVFX;
-    public float timerForExplotion;
+    public GameObject[] meshToDisable;
+
+    public float timerForExplotion = 5f;
+
+    public float timerToDestroyObject = 2f;
+
+    public bool destroyObject;
+
+    private Rigidbody rb;
 
     private void Start() {
-        
+        rb = transform.GetComponent<Rigidbody>();
     }
 
     private void Update() {
@@ -68,11 +31,41 @@ public class TumbleWeed : MonoBehaviour
             explotionBuildUpVFX.SetActive(false);
             // Then we play Explotion Effect
             TumbleWeedExplotionVFX.SetActive(true);
-        }        
+
+            foreach (var Objects in meshToDisable)
+            {
+                Objects.SetActive(false);
+            }
+
+            destroyObject = true;
+        }
+
+        if (destroyObject)
+        {
+            rb.velocity = new Vector3(0,0,0);
+
+            if (timerToDestroyObject > 0)
+            {
+                timerToDestroyObject -= Time.deltaTime;      
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }   
+        }
+
     }
 
     private void OnTriggerEnter(Collider other) {
-        explotionBuildUpVFX.SetActive(false);
-        TumbleWeedExplotionVFX.SetActive(true);
+        if(other.CompareTag("Enemy"))
+        {
+            explotionBuildUpVFX.SetActive(false);
+            TumbleWeedExplotionVFX.SetActive(true);
+                foreach (var Objects in meshToDisable)
+                {
+                    Objects.SetActive(false);
+                }
+            destroyObject = true;
+        }
     }
 }
