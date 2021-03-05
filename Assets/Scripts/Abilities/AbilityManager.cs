f3da9c3599de9d78448c4afc5e3355727d563bdc
TheDevArray <Christian.kollbaer@gmail.com>
2021-03-05 15:10:48 +0100

    FailedOnMakingBlendShader

--- Assets/Scripts/Abilities/AbilityManager.cs
+++ Assets/Scripts/Abilities/AbilityManager.cs
@@ -20,6 +20,16 @@ public class AbilityManager : MonoBehaviour
     public float tumbleSpeed;
     public float radius;
 
+    [Header("Jungle Ability Values")]
+    public Material newMat;
+    public float jungleAbilityCooldown;
+    public float camuflageSpeed;
+    public float camouflageCooldown;
+    private Renderer rend;
+    private Material oldMat;
+    public bool isCamoflaged;
+
+
     private Vector3 playerOffset;
     private Camera mainCamera;
 
@@ -29,6 +39,10 @@ public class AbilityManager : MonoBehaviour
         instance = this;
 
         mainCamera = Camera.main;
+
+        rend = transform.GetComponent<Renderer>();
+
+        oldMat = rend.GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
@@ -40,6 +54,7 @@ public class AbilityManager : MonoBehaviour
         {
             case "MainIsland":
                 TumbleWeedAbility();
+                CamouflageAbility();
                 break;
             
             case "Desert":
@@ -59,7 +74,7 @@ public class AbilityManager : MonoBehaviour
                 break;
             
             case "Jungle":
-                
+                CamouflageAbility();
                 break;
         }
         
@@ -68,6 +83,11 @@ public class AbilityManager : MonoBehaviour
         if (tumbleWeedCooldown > 0)
         {
             tumbleWeedCooldown -= Time.deltaTime;
+        }
+
+        if(camouflageCooldown > 0)
+        {
+            camouflageCooldown -= Time.deltaTime;
         }
     }
 
@@ -83,6 +103,30 @@ public class AbilityManager : MonoBehaviour
             tumbleWeedObjectInstancer.GetComponent<SphereCollider>().radius = radius;
             
             tumbleWeedCooldown = desertAbilityCooldown;
+        }
+    }
+
+
+    public void CamouflageAbility()
+    {
+
+        if(Input.GetKey(KeyCode.F) && camouflageCooldown <= 0 && !isCamoflaged)
+        {
+            Debug.Log("Camoflaged :D");
+
+            rend.material.Lerp(oldMat, newMat, camuflageSpeed * Time.deltaTime);
+
+            //isCamoflaged = true;
+            //camouflageCooldown = jungleAbilityCooldown;
+        }
+        if(Input.GetKey(KeyCode.F) && isCamoflaged && camouflageCooldown <= 0)
+        {
+            Debug.Log("Visable :D");
+
+            rend.material.Lerp(newMat, oldMat, camuflageSpeed * Time.deltaTime);
+
+            camouflageCooldown = jungleAbilityCooldown;
+            isCamoflaged = false;
         }
     }
 }
