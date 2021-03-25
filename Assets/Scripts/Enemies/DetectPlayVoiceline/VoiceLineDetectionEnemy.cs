8c487880b19a777e837e2187d4e0ca5b840e79b3
unknown <57574585+TheDevArray@users.noreply.github.com>
2021-03-25 11:56:14 +0100

    FirstEnemyComplete

--- Assets/Scripts/Enemies/DetectPlayVoiceline/VoiceLineDetectionEnemy.cs
+++ Assets/Scripts/Enemies/DetectPlayVoiceline/VoiceLineDetectionEnemy.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
+[RequireComponent(typeof(AudioSource))]
 public class VoiceLineDetectionEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private AudioSource audioSource;
+
+    private Boolean hasBeenDetected;
+
+    private GameObject target;
+    
+    public float viewDistance;
+    
+    [FormerlySerializedAs("rotSpeed")] public float returnSpeed;
+
+    private Quaternion startRot;
+    
+    private void Start()
     {
-        
+        startRot = transform.rotation;
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (hasBeenDetected && target)
+        {
+            if (Vector3.Distance(target.transform.position, transform.position) <= viewDistance)
+            {
+                transform.rotation = Quaternion.LookRotation(target.transform.position);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, startRot, returnSpeed * Time.deltaTime);
+            }
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            target = other.gameObject;
+            if (!hasBeenDetected)
+            {
+                audioSource.Play();
+                hasBeenDetected = true;
+            }
+
+        }
     }
 }
