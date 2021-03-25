ac03a18b1887077fe535074e1834a9fa981824ff
unknown <57574585+TheDevArray@users.noreply.github.com>
2021-03-25 11:26:48 +0100

    SpinningPlatform

---
+++ Assets/Scripts/Platform/SpinScript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScript : MonoBehaviour
+{
+    public float spinSpeed = 56f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,spinSpeed * Time.deltaTime,0);
+    }
+}
