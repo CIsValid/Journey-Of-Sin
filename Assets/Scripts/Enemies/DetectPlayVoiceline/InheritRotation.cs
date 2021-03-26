4b4a3b2688042eb60646c8760681e44475b9b4a6
unknown <57574585+TheDevArray@users.noreply.github.com>
2021-03-26 11:24:33 +0100

    SpinningGhosts

---
+++ Assets/Scripts/Enemies/DetectPlayVoiceline/InheritRotation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InheritRotation : MonoBehaviour
+{
+    public Transform targetRotation;
+
+    private void Update()
+    {
+        transform.Rotate(targetRotation.rotation.x,targetRotation.rotation.y, transform.rotation.z);
+    }
+}
