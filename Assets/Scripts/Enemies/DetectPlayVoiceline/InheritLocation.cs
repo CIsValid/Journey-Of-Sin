43b38f89dc6e422ff47936791908ae96dba18691
unknown <57574585+TheDevArray@users.noreply.github.com>
2021-03-26 11:28:37 +0100

    inheritLoc

---
+++ Assets/Scripts/Enemies/DetectPlayVoiceline/InheritLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InheritLocation : MonoBehaviour
+{
+    public Transform targetLoc;
+
+    private void Update()
+    {
+        transform.position = new Vector3(targetLoc.position.x, targetLoc.position.y, transform.position.z);
+    }
+}
